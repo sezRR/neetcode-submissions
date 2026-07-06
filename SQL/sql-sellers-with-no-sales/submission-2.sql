d75dfc1e463e6ba9caf8d3b6d7618c13e2b03fa2
Sezer Tetik <me@sezertetik.dev>
2026-07-06 23:20:12 +0300

    Add: sql-sellers-with-no-sales - submission-2

---
+++ SQL/sql-sellers-with-no-sales/submission-2.sql
@@ -0,0 +1,6 @@
+SELECT s.seller_name FROM seller s
+WHERE NOT EXISTS(
+    SELECT 1 FROM orders oo
+    WHERE oo.seller_id = s.seller_id AND EXTRACT(YEAR FROM oo.sale_date) = 2020
+)
+ORDER BY s.seller_name;
