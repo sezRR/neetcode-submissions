0165d069d442774fe06be0033b66a9d8a4992173
Sezer Tetik <me@sezertetik.dev>
2026-07-07 14:24:29 +0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack and self.minStack[-1] >= val:
+            self.minStack.append(val)
+        else:
+            self.minStack.appendleft(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val > self.minStack[-1]:
+            self.minStack.popleft()
+        else:
+            self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
